84552cd8014c3f712026d01eac5a5a3dd615f438
Nursaadet <nursaadetozdemir@gmail.com>
2026-02-04 19:50:40 +0300

    feat: add hero section with movie trailer video

---
+++ src/app/(private)/movies/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import VideoSection from "./VideoSection";
+import { getVideoKey } from "@/helpers/movieFunctions";
+
+const HeroSection = async ({ id }) => {
+  const videoKey = await getVideoKey(id);
+  console.log(videoKey);
+  return (
+    <div className="relative h-[50vw]">
+      <VideoSection videoKey={videoKey} />
+    </div>
+  );
+};
+
+export default HeroSection;
