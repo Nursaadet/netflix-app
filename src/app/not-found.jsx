c1a3df4904ea4575a244e2227a2197297c85f388
Nursaadet <nursaadetozdemir@gmail.com>
2026-02-04 16:55:37 +0300

    feat: add home page hero section and custom 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <img src="/images/404-page-not-found.png" alt="" />
+      <button className="">Go Back</button>
+    </div>
+  );
+};
+
+export default NotFound;
