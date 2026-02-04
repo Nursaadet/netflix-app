9bc75e7fed6e0776b8d7a8ee09f6eec5fc96363b
Nursaadet <nursaadetozdemir@gmail.com>
2026-02-04 20:22:27 +0300

    feat: add dynamic metadata for movie pages

--- src/app/(private)/movies/[movieId]/page.jsx
+++ src/app/(private)/movies/[movieId]/page.jsx
@@ -27,3 +27,11 @@ const MovieDetail = async ({ params: { movieId } }) => {
 };
 
 export default MovieDetail;
+
+export async function generateMetadata({ params: { movieId } }) {
+  const movieDetails = await getMovieDetail(movieId);
+  return {
+    title: movieDetails.title,
+    description: `This is the page of ${movieDetails.title}`,
+  };
+}
